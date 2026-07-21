ee2442e6701cec0b2642884b9bd6b62f524e8fd0
cepas-apuntes <mati@helman.com.ar>
2026-07-21 20:01:06 -0300

    Update 2 firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,6 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvB0FD6h9Qz2u9cjFzZYtjSLqnX2rFDY4",
@@ -24,5 +23,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
